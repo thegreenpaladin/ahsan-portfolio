a2dbc7badb96df62d3566c3cf84f8b3cb727a217
Muhammad Ahsan <ahsanmuhammed45@gmail.com>
2024-06-26 03:35:12 +0500

    update content

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
       <div className="">
         <h1 className="text-3xl font-bold">Muhammad Ahsan</h1>
         <h3 className="text-lg mt-5">
-          I'm a Software Engineer and Programmer.
+          I build web apps using JavaScript and TypeScript.
         </h3>
-        <button className=" bg-slate-800 mt-10 p-3 border-2 rounded-sm font-medium border-white hover:scale-105 transition ease-out duration-300">
+        <a className=" bg-slate-800 mt-10 p-3 border-2 rounded-sm font-medium border-white hover:scale-105 transition ease-out duration-300">
           View Work
-        </button>
+        </a>
       </div>
     </main>
   );
